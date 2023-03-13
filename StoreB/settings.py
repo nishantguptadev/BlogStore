1e672e315fbad80f0cfcce84fa5f2d2c1b5817c5
Nishant Gupta <nishantgupta.dev@gmail.com>
2023-03-13 06:44:05 +0530

    .txt

--- StoreB/settings.py
+++ StoreB/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants as messages
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,11 +125,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static")
 ]
 
 MEDIA_URL='/media/'
-MEDIA_ROOT= BASE_DIR / "static"
+MEDIA_ROOT= os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
